c1aa425dae8372b07dfdbbb5370bc382c5be2586
Nate Hodge <nhodge253@gmail.com>
2019-10-30 22:58:25 -0700

    deployed React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,19 +4,14 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 
     function RenderMenuItem({ dish, onClick}) {
         return (
-            <Card onClick={() => onClick(dish.id)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
+            <Card></Card>
         );
     }
     const Menu = (props) => {
         const menu = props.dishes.map((dish) => { 
             return (
                 <div className="col-12 col-md-5 m-1" key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                    <RenderMenuItem dish={dish}/>
               </div>
             );
         });
