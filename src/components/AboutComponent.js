e790c7ea089f094ee9e417e2f3a182ef395b4450
Nate Hodge <nhodge253@gmail.com>
2019-11-03 12:38:28 -0800

    got rid of reference code

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -20,23 +20,6 @@ function RenderLeader({ leader }) {
       </div>
     );
   }
-
-  /* render() {
-        const menu = this.state.dishes.map((dish) => {
-            return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
-              </div>
-            );
-        });*/
   
 function About(props) {
 
