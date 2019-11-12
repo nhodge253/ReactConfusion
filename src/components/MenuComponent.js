552cb0bfe5cefef5a78afae0c3e0552109f1b035
Nate Hodge <nhodge253@gmail.com>
2019-11-11 18:02:54 -0800

    added redux thunk and logger. Deployed loading and error message thunks

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {
+    Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
-    function RenderMenuItem({ dish, onClick}) {
-        return (
-            <Card>
-                <Link to={`/menu/${dish.id}`}>
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
-                </Link>
-            </Card>
+            </Link>
+        </Card>
+    );
+}
+const Menu = (props) => {
+    const menu = props.dishes.dishes.map((dish) => {
+        return (
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenuItem dish={dish} />
+            </div>
+        );
+    });
+
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
         );
     }
-    const Menu = (props) => {
-        const menu = props.dishes.map((dish) => { 
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                <RenderMenuItem dish={dish}/>
+    else if (props.dishes.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
             </div>
-            );
-        });
-
+        );
+    }
+    else
         return (
             <div className="container">
                 <div className="row">
@@ -41,7 +63,7 @@ import { Link } from 'react-router-dom';
                 </div>
             </div>
         );
-    }
+}
 
 
 export default Menu;
